04e2ebb1d681fd4f54c0aa62f4537ef1c6148c13
Eduardo Felizardo <edufelizardo1@gmail.com>
2021-10-13 19:56:09 -0300

    Update Cliente.java

--- desafio-dio-banco/src/Cliente.java
+++ desafio-dio-banco/src/Cliente.java
@@ -1,6 +1,14 @@
 
 public class Cliente {
 
-	String nome;
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 	
 }
