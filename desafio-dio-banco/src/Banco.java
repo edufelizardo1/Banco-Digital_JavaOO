aac258497fb4c5a07757974fa86b15a48ca30c6d
Eduardo Felizardo <edufelizardo1@gmail.com>
2021-10-13 19:55:58 -0300

    Update Banco.java

--- desafio-dio-banco/src/Banco.java
+++ desafio-dio-banco/src/Banco.java
@@ -1,5 +1,15 @@
 
 public class Banco {
 
-	String nome;
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
 }
