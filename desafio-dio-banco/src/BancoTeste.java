bbfd26e75d8052bb1674066c3973faf9074ebea7
Eduardo Felizardo <edufelizardo1@gmail.com>
2021-10-14 07:26:35 -0300

    Create BancoTeste.java

---
+++ desafio-dio-banco/src/BancoTeste.java
@@ -0,0 +1,13 @@
+
+public class BancoTeste {
+
+	public static void main(String[] args) {
+
+		Conta cc = new ContaCorrente();
+		Conta cp = new ContaPoupanca();
+		
+		cc.imprimirextato();
+		cp.imprimirextato();
+	}
+
+}
