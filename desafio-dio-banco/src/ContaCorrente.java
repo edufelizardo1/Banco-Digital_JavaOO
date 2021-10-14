10951d992896d21fd67b52440d5948dfab5273ba
Eduardo Felizardo <edufelizardo1@gmail.com>
2021-10-14 07:26:48 -0300

    Update ContaCorrente.java

--- desafio-dio-banco/src/ContaCorrente.java
+++ desafio-dio-banco/src/ContaCorrente.java
@@ -1,5 +1,8 @@
 
 public class ContaCorrente extends Conta{
 
-			
+	public void imprimirextato() {
+		System.out.println("*** Extarto Conta Corrente ***");
+		super.imprimirAtributosComuns();
+	}		
 }
