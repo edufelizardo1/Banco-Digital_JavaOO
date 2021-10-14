1a89c1fb24c8f54fcc200fa1a233cc3155242d57
Eduardo Felizardo <edufelizardo1@gmail.com>
2021-10-14 07:26:55 -0300

    Update ContaPoupanca.java

--- desafio-dio-banco/src/ContaPoupanca.java
+++ desafio-dio-banco/src/ContaPoupanca.java
@@ -1,5 +1,10 @@
 
 public class ContaPoupanca  extends Conta{
 	
+	public void imprimirextato() {
+		System.out.println("*** Extarto Conta Poupança ***");
+		super.imprimirAtributosComuns();
+	}
+
 	
 }
